209464b5ba1eecde9bf7bfc88a6a810cd374585f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-21 01:30:14 -0600

    Edit validation message of reminder model

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,6 +1,6 @@
 class Reminder < ApplicationRecord
   # validations
-  validates :description, presence: true, length: { maximum: 30, too_long: 'Too long. Maximum is 1200 characters.' }
+  validates :description, presence: true, length: { maximum: 30, too_long: 'Too long. Maximum is 30 characters.' }
   validates :datetime, presence: true
   validates :city, presence: true, length: { maximum: 60 }
   validates :location_coordinates, presence: true, length: { maximum: 100 }
