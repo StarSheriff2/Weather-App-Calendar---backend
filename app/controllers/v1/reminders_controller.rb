86043337c7ec4bde53356baf794bbbb139c8b89d
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-21 16:35:29 -0600

    Create API versioning file, seed file with 50+ reminders for first user, update routes to include namaspaced api

---
+++ app/controllers/v1/reminders_controller.rb
@@ -0,0 +1,45 @@
+module V1
+  class RemindersController < ApplicationController
+    before_action :set_reminder, only: %i[show update destroy]
+
+    # GET /reminders
+    def index
+      @reminders = current_user.reminders
+      json_response(@reminders)
+    end
+
+    # POST /reminders
+    def create
+      @reminder = current_user.reminders.create!(reminder_params)
+      json_response(@reminder, :created)
+    end
+
+    # GET /reminders/:id
+    def show
+      json_response(@reminder)
+    end
+
+    # PUT /reminders/:id
+    def update
+      @reminder.update(reminder_params)
+      head :no_content
+    end
+
+    # DELETE /reminders/:id
+    def destroy
+      @reminder.destroy
+      head :no_content
+    end
+
+    private
+
+    def reminder_params
+      # whitelist params
+      params.require(:reminder).permit(:description, :datetime, :city, :location_coordinates)
+    end
+
+    def set_reminder
+      @reminder = Reminder.find(params[:id])
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# seed 50 records
+50.times do
+  reminder = Reminder.create(
+    description: Faker::Lorem.characters(number: 30),
+    datetime: Faker::Time.forward(days: 364),
+    city: Faker::Address.city,
+    location_coordinates: "#{Faker::Address.latitude}, #{Faker::Address.longitude}",
+    created_by: User.first.id
+  )
+end
