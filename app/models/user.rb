2a6daf02c80745972f22baa1e7cde88b1b72ea31
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-21 20:01:22 -0600

    Remove uniqueness constraint from user name value

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Model associations
   has_many :reminders, dependent: :destroy, foreign_key: :created_by
   # Validations
-  validates :name, presence: true, length: { maximum: 35 }, uniqueness: true
+  validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 24 }
   validates :password_digest, presence: true
 end
