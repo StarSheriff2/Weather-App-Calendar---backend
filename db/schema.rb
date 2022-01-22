681af513575854bf6f433e3f6bc1c800205891d5
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-21 20:37:36 -0600

    Add index to email column in user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_095643) do
+ActiveRecord::Schema.define(version: 2022_01_22_023239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2022_01_21_095643) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
