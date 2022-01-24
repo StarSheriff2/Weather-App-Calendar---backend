8369d21519cb8ac1a8b4699498b75984cab11dc2
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-24 03:11:12 -0600

    Modify length of reminder description in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create(name: 'Test User', email: 'foo@bar.com', password: 'foobar44A33!')
 # seed 50 records
 50.times do
   reminder = Reminder.create(
-    description: Faker::Lorem.characters(number: 30),
+    description: Faker::Lorem.words(number: rand(2..3)).join(' '),
     datetime: Faker::Time.forward(days: 364),
     city: Faker::Address.city,
     location_coordinates: "#{Faker::Address.latitude}, #{Faker::Address.longitude}",
