c21c136beecd79ac585e3a01757a16c0475b135c
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-26 19:16:04 -0600

    changed generated reminders in seed file to 20 records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.create(name: 'Test User', email: 'foo@bar.com', password: 'foobar44A33!')
 # seed 50 records
-50.times do
+20.times do
   reminder = Reminder.create(
     description: Faker::Lorem.words(number: rand(2..3)).join(' '),
     datetime: Faker::Time.forward(days: 364),
