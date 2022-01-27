a1caa118b534bbf51a08173d3b0c15b5731d8cb3
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-26 19:22:26 -0600

    Migrate and seed data locally

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create(name: 'Test User', email: 'foo@bar.com', password: 'foobar44A33!')
-# seed 50 records
+# seed 20 records
 20.times do
   reminder = Reminder.create(
     description: Faker::Lorem.words(number: rand(2..3)).join(' '),
