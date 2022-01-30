e5f48894ea642d69a54eb45db57aea8bd7b639e9
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-30 01:59:10 -0600

    Add Production Domain of frontend to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -22,12 +22,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
-  # allow do
-  #   origins 'https://expert-class-frontend-v2.netlify.app'
+  allow do
+    origins 'https://weather-app-calendar.netlify.app'
 
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 end
