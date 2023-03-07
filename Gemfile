a02a564e570de8f6a7fe3688f63d75b7df71cdfd
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2023-03-07 14:09:15 -0600

    Add health check endpoint

--- Gemfile
+++ Gemfile
@@ -41,6 +41,11 @@ gem 'jwt'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
+# Health Check route (for production)
+gem 'rails-healthcheck'
+
+gem 'dotenv-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
---
+++ config/initializers/healthcheck.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'dotenv'
+Dotenv.load
+
+Healthcheck.configure do |config|
+  config.success = 200
+  config.error = 503
+  config.verbose = true
+  config.route = '/healthcheck'
+  config.method = :get
+
+  # -- Checks --
+  config.add_check :database,     -> { ActiveRecord::Base.connection.execute('SELECT 1') }
+  config.add_check :migrations,   -> { ActiveRecord::Migration.check_pending! }
+  config.add_check :environments, -> { Dotenv.require_keys('DATABASE_URL', 'RAILS_MASTER_KEY', 'SECRET_KEY_BASE') unless ENV['RAILS_ENV'] == 'development' }
+end
