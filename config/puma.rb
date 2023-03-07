9ca74a76a2cd5fd6b9c9a54cb5222e2d9fbf8afd
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2023-03-06 21:29:55 -0600

    Fix if statements

--- config/puma.rb
+++ config/puma.rb
@@ -31,14 +31,14 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 #
-if ENV['RENDER'].present? workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 } if ENV['RENDER'].present?
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-if ENV['RENDER'].present? preload_app!
+preload_app! if ENV['RENDER'].present?
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
