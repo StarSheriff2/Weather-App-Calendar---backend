ef7a80045c4ad319024a01897b3e34b959ef6875
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2023-03-07 18:08:43 -0600

    remove dotenv-rails gem

--- config/initializers/healthcheck.rb
+++ config/initializers/healthcheck.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require 'dotenv'
-Dotenv.load
-
 Healthcheck.configure do |config|
   config.success = 200
   config.error = 503
@@ -13,5 +10,4 @@ Healthcheck.configure do |config|
   # -- Checks --
   config.add_check :database,     -> { ActiveRecord::Base.connection.execute('SELECT 1') }
   config.add_check :migrations,   -> { ActiveRecord::Migration.check_pending! }
-  config.add_check :environments, -> { Dotenv.require_keys('DATABASE_URL', 'RAILS_MASTER_KEY', 'SECRET_KEY_BASE') unless ENV['RAILS_ENV'] == 'production' }
 end
