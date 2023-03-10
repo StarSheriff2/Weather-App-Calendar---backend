fb832d2128f5d4b87fa3a7cbc04895cb71a9de3b
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2023-03-10 00:55:36 -0600

    try moving healthcheck path elsewhere

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
 
   scope module: :v1, :defaults => {:format => :json}, constraints: ApiVersion.new('v1', default: true) do
     resources :reminders
+
+    get '/healthcheck', to: proc { [200, {}, ['OK']] }
   end
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   # get  '/healthcheck', to: 'healthcheck#check'
-  get '/healthcheck', to: proc { [200, {}, ['OK']] }
+  root to: proc { [200, {}, ['nothing here']] }
 end
